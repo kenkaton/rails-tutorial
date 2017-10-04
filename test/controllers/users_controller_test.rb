36f52d5150a63e436a65385afbcb86b0809e3c2d
kenkat0 <kny.mkdt.12@gmail.com>
2017-10-04 11:09:32 +0000

    Make date model of account activation

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -17,7 +17,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
   
-   test "should redirect edit when not logged in" do
+  test "should redirect edit when not logged in" do
     get edit_user_path(@user)
     assert_not flash.empty?
     assert_redirected_to login_url
